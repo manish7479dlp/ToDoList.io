c6d998afff7b50b95714192aed8930ec9c32a938
Manish Kumar <manish7479dlp@gmail.com>
2022-01-04 14:45:03 +0530

    UI done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'
 
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='main_container'>
+        <div className='container'>
+          <h1 className='heading'>ToDo List</h1>
+          <br/>
+          <input className='input' type= "text" placeholder='Enter Your Task..'/>
+          <button className='addbtn'>+</button>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App;
